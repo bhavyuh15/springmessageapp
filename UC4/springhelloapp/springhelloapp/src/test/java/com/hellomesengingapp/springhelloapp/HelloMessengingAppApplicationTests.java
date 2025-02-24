c7c87204c17342785aec02e9f29140bf1cc70b34
bhavyuh15 <bhavyadatey15@gmail.com>
2025-02-25 01:49:37 +0530

    [Bhavya] added user case 4 in UC4 Branch

---
+++ UC4/springhelloapp/springhelloapp/src/test/java/com/hellomesengingapp/springhelloapp/HelloMessengingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hellomesengingapp.springhelloapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMessengingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
