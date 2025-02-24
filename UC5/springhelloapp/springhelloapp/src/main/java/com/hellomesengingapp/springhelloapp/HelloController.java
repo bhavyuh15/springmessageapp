02d718a2aa8f63022da955b35b88fe4ee976ae76
bhavyuh15 <bhavyadatey15@gmail.com>
2025-02-25 01:53:16 +0530

    [Bhavya] added user case 5 in UC5 Branch

---
+++ UC5/springhelloapp/springhelloapp/src/main/java/com/hellomesengingapp/springhelloapp/HelloController.java
@@ -0,0 +1,68 @@
+package com.hellomesengingapp.springhelloapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+// User DTO Bean
+class User {
+    private String firstName;
+    private String lastName;
+
+    // Getters and Setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    // GET Request Method
+    @GetMapping
+    public String sayHello() {
+        return "Hello from BridgeLabz";
+    }
+
+    // GET Request Method with Query Parameter
+    @GetMapping("/query")
+    public String sayHelloWithName(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+
+    // GET Request Method with Path Variable
+    @GetMapping("/param/{name}")
+    public String sayHelloWithPath(@PathVariable String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+
+    // POST Request Method with Request Body
+    @PostMapping("/post")
+    public String sayHelloWithFullName(@RequestBody User user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
+    }
+
+    // PUT Request Method with Path Variable and Query Parameter
+    @PutMapping("/put/{firstName}")
+    public String sayHelloWithPut(@PathVariable String firstName, @RequestParam String lastName) {
+        return "Hello " + firstName + " " + lastName + " from BridgeLabz";
+    }
+}
